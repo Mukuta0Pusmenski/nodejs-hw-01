f120f9201b0f00559cb2d3addb517a233ea29e59
Mukuta0Pusmenski <167695339+Mukuta0Pusmenski@users.noreply.github.com>
2025-03-23 21:22:37 +0200

    push=002

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,3 @@
-export const PATH_DB = './src/db/db.json';
+import { resolve } from 'path';
+
+export const PATH_DB = resolve('./src/db/db.json');
--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,21 @@
-export const addOneContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const addOneContact = async () => {
+  try {
+    const contacts = await readContacts();
+
+    const newContact = createFakeContact();
+
+    const updatedContacts = [...contacts, newContact];
+
+    await writeContacts(updatedContacts);
+
+    console.log('One contact added successfully!');
+  } catch (error) {
+    console.error('Error adding contact:', error.message);
+  }
+};
 
 addOneContact();
